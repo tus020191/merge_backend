6ed510116ae542d01efec2643e3297e3af6a8ce2
Tushar <tus123kumar@gmail.com>
2026-08-05 09:22:47 +0530

    added user using custome command management and used ajax to update like counter value

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post
 from django.contrib.auth.admin import UserAdmin
-from .models import User , Category , Tag , Comment
+from .models import User , Category , Tag , Comment , Like
 
 admin.site.register(Post)
 
@@ -12,6 +12,8 @@ admin.site.register(Tag)
 
 admin.site.register(Comment)
 
+
+admin.site.register(Like)
 
 
 @admin.register(User)
